19dcbe1ebc529f25104ce79d6b922b5d8b3a2f4e
pravalikavallapu <30765784+pravalikavallapu@users.noreply.github.com>
2017-08-10 16:29:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,8 @@
-console.log('Loaded!');
-var element=document.getElementBy('main-text');
-
-element.innerHTML='New Value';
+var button = document.getElementBy('counter');
+var counter=0;
+button.onclick=function()
+{
+    counter=counter+1;
+    var span=document.getElementBy('count');
+    span.innerHTML=counter.toString();
+}
